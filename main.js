bb14a9415225ad88dca3fb4631b8ab4ac01c045a
EsraaGamal404 <eg6237561@gmail.com>
2024-05-17 13:21:57 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// let a = document.getElementById("title")
+// a.innerHTML="hallo esraa";
+// const a = document.createElement("a");
+// a.innerHTML = "school website ";
+// document.body.append(a);
